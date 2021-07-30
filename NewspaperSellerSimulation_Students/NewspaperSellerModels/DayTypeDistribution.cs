2cd69c7d88227b791248c2d6f625a72e977cd0f0
Ahmed Mostafa <37251855+AhmedNobi@users.noreply.github.com>
2021-07-30 02:18:24 +0200

    Add files via upload

---
+++ NewspaperSellerSimulation_Students/NewspaperSellerModels/DayTypeDistribution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewspaperSellerModels
+{
+    public class DayTypeDistribution
+    {
+        public DayTypeDistribution(Enums.DayType DayType_, decimal Probability_)
+        {
+            this.DayType = DayType_;
+            this.Probability = Probability_;
+        }
+        public DayTypeDistribution()
+        {
+            this.MinRange = 0;
+            this.MaxRange = 0;
+            this.Probability = 0;
+            this.CummProbability = 0;
+        }
+        public Enums.DayType DayType { get; set; }
+        public decimal Probability { get; set; }
+        public decimal CummProbability { get; set; }
+        public int MinRange { get; set; }
+        public int MaxRange { get; set; }
+        
+    }
+}
